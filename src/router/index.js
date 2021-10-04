32a9aa3973951636ec736e029cba358b1e9fddb4
vihvch <vihvch@gmail.com>
2021-10-04 22:18:38 +0800

    創建用戶登入頁面組件並配置路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,19 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+// 在 Vue CLI 創建的項目中, @ 表示 src 目錄
+import Login from '@/views/login'
+
 Vue.use(VueRouter)
 
 // 路由配置表
-const routes = []
+const routes = [
+  {
+    path: '/login',
+    name: 'login',
+    component: Login
+  }
+]
 
 const router = new VueRouter({
   routes
