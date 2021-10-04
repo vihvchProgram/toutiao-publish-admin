92204ff9766be7938dbae4a452dcbe38fa98d269
vihvch <vihvch@gmail.com>
2021-10-04 23:29:20 +0800

    用戶登入頁面布局調整

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,16 +1,28 @@
 <template>
   <div class="login-container">
-    <el-form ref="form" :model="user" label-width="80px">
-      <el-form-item label="手機號">
-        <el-input v-model="user.mobile"></el-input>
-      </el-form-item>
-      <el-form-item label="驗證碼">
-        <el-input v-model="user.code"></el-input>
-      </el-form-item>
-      <el-form-item>
-        <el-button type="primary" @click="onSubmit">登入</el-button>
-      </el-form-item>
-    </el-form>
+    <div class="login-form-wrap">
+      <div class="login-head">
+        <div class="logo"></div>
+      </div>
+      <el-form class="login-form" ref="form" :model="user">
+        <el-form-item>
+          <el-input v-model="user.mobile"
+                    placeholder="請輸入手機號"
+          ></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input v-model="user.code"
+                    placeholder="請輸入驗證碼"
+          ></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-checkbox v-model="user.agree">我已閱讀並同意用戶協議和隱私條款</el-checkbox>
+        </el-form-item>
+        <el-form-item>
+          <el-button class="login-btn" type="primary" @click="onSubmit">登入</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
   </div>
 </template>
 
@@ -23,7 +35,8 @@ export default {
     return {
       user: {
         mobile: '', // 手機號
-        code: '' // 驗證碼
+        code: '', // 驗證碼
+        agree: false // 是否同意協議的選中狀態
       }
     }
   },
@@ -39,4 +52,38 @@ export default {
 }
 </script>
 
-<style scoped lang="less"></style>
+<style scoped lang="less">
+.login-container {
+  position: fixed;
+  left: 0;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background: url("./login_bg.jpg") no-repeat;
+  background-size: cover;
+  .login-form-wrap {
+    min-width: 300px;
+    padding: 30px 50px 10px;
+    background-color: #fff;
+    .login-head {
+      display: flex;
+      justify-content: center;
+      .logo {
+        width: 200px;
+        height: 57px;
+        background: url("./logo_index.png") no-repeat;
+        background-size: contain;
+      }
+    }
+    .login-form {
+      .login-btn {
+        width: 100%;
+      }
+    }
+  }
+}
+</style>
