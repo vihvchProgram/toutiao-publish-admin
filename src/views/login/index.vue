9c856c0d7b5727a7ec6ebcf0314d7328d9f2cd2b
vihvch <vihvch@gmail.com>
2021-10-05 23:59:07 +0800

    表單驗證的基本使用

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -4,18 +4,25 @@
       <div class="login-head">
         <div class="logo"></div>
       </div>
-      <el-form class="login-form" ref="form" :model="user">
-        <el-form-item>
+      <!--
+        配置 Form 表單驗證:
+        1. 必須給 el-form組件 綁定model為 表單數據對象
+        2. 給需要驗證的表單項el-form-item 綁定 prop屬性
+          注意: prop屬性 需要指定為 表單對象中的 數據名稱
+        3. 通過 el-form組件的rules屬性 配置驗證規則
+      -->
+      <el-form class="login-form" ref="form" :model="user" :rules="formRules">
+        <el-form-item prop="mobile">
           <el-input v-model="user.mobile"
                     placeholder="請輸入手機號"
           ></el-input>
         </el-form-item>
-        <el-form-item>
+        <el-form-item prop="code">
           <el-input v-model="user.code"
                     placeholder="請輸入驗證碼"
           ></el-input>
         </el-form-item>
-        <el-form-item>
+        <el-form-item prop="agree">
           <el-checkbox v-model="user.agree">我已閱讀並同意用戶協議和隱私條款</el-checkbox>
         </el-form-item>
         <el-form-item>
@@ -41,11 +48,22 @@ export default {
   data () {
     return {
       user: {
-        mobile: '13911111111', // 手機號
-        code: '246810', // 驗證碼
+        mobile: '', // 手機號
+        code: '', // 驗證碼
         agree: false // 是否同意協議的選中狀態
       },
-      loginLoading: false // 登入的 loading 狀態
+      loginLoading: false, // 登入的 loading 狀態
+      formRules: { // 表單驗證的 規則配置
+        // 要驗證的數據名稱: 規則列表[]
+        mobile: [
+          { required: true, message: '請輸入手機號', trigger: 'change' },
+          { pattern: /^1[3|5|7|8|9]\d{9}$/, message: '請輸入正確的手機號碼格式', trigger: 'change' }
+        ],
+        code: [
+          { required: true, message: '驗證碼不能為空', trigger: 'change' },
+          { pattern: /^\d{6}$/, message: '請輸入正確的驗證碼格式', trigger: 'change' }
+        ]
+      }
     }
   },
   computed: {},
