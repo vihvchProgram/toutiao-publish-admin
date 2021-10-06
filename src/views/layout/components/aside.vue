c8c54f54c42630926e430379248c2ec18ac1fdab
vihvch <vihvch@gmail.com>
2021-10-07 01:44:24 +0800

    設置側邊欄導航菜單的路由

--- src/views/layout/components/aside.vue
+++ src/views/layout/components/aside.vue
@@ -1,35 +1,41 @@
 <template>
+    <!--
+      el-menu 的 router 屬性 :
+        1. 是否使用 vue-router 的模式
+        2. 啟用該模式會在激活導航時以 index 作為 path 進行路由跳轉
+    -->
     <el-menu
-      default-active="2"
-      class="el-menu-vertical-demo"
-      background-color="#545c64"
+      default-active="/"
+      background-color="#002033"
       text-color="#fff"
-      active-text-color="#ffd04b">
-      <el-menu-item index="1">
-        <i class="el-icon-location"></i>
+      active-text-color="#ffd04b"
+      router
+    >
+      <el-menu-item index="/">
+        <i class="el-icon-s-home"></i>
         <span slot="title">首頁</span>
       </el-menu-item>
-      <el-menu-item index="2">
+      <el-menu-item index="/article">
         <i class="el-icon-menu"></i>
         <span slot="title">內容管理</span>
       </el-menu-item>
-      <el-menu-item index="3">
-        <i class="el-icon-menu"></i>
+      <el-menu-item index="/image">
+        <i class="el-icon-picture"></i>
         <span slot="title">素材管理</span>
       </el-menu-item>
-      <el-menu-item index="4">
-        <i class="el-icon-document"></i>
+      <el-menu-item index="/publish">
+        <i class="el-icon-edit-outline"></i>
         <span slot="title">發布文章</span>
       </el-menu-item>
-      <el-menu-item index="5">
-        <i class="el-icon-menu"></i>
+      <el-menu-item index="comment">
+        <i class="el-icon-document-copy"></i>
         <span slot="title">評論管理</span>
       </el-menu-item>
-      <el-menu-item index="6">
-        <i class="el-icon-menu"></i>
+      <el-menu-item index="fans">
+        <i class="el-icon-s-custom"></i>
         <span slot="title">粉絲管理</span>
       </el-menu-item>
-      <el-menu-item index="7">
+      <el-menu-item index="settings">
         <i class="el-icon-setting"></i>
         <span slot="title">個人設置</span>
       </el-menu-item>
