5b5e5e8a05fc24e698aa2c7bc245f2bf70b4f561
vihvch <vihvch@gmail.com>
2021-10-30 10:09:15 +0800

    通過本地存儲 拿到用戶信息, 以便獲取 token數據

--- src/api/user.js
+++ src/api/user.js
@@ -15,10 +15,13 @@ export const login = data => {
 
 // 獲取用戶信息
 export const getUserProfile = () => {
-  // return 1
+  // 通過本地存儲 拿到用戶信息, 以便獲取 token數據
+  // 將拿到的JSON格式字符串還原回 原來的數據對象
+  const user = JSON.parse(window.localStorage.getItem('user'))
+  console.log('本地存儲', user)
   return request({
     method: 'GET',
-    usr: '/mp/v1_0/user/profile',
+    url: '/mp/v1_0/user/profile',
     // 後端要求 把需要授權的用戶身分 放到請求頭信息 中
     // axios 可以通過 headers 配置項 來設置請求頭信息
     headers: {
@@ -26,7 +29,7 @@ export const getUserProfile = () => {
       // 1) 屬性名: Authorization, 後端接口 要求的
       // 2) 屬性值: Bearar[空格][token數據]
       // Bearar: 持票人
-      Authorization: 'Bearar eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2N…J1ZX0.ZNtBkuPtZmkPrLUo29CNgsd4rIXO1Mu_NoVIpmZrtAA'
+      Authorization: `Bearer ${user.token}`
     }
   })
 }
