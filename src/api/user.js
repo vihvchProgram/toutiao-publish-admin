86b9268a0800b6ae6be689a9458d17feaf244555
vihvch <vihvch@gmail.com>
2021-10-30 11:24:30 +0800

    使用請求攔截器，統一設置 token

--- src/api/user.js
+++ src/api/user.js
@@ -17,19 +17,19 @@ export const login = data => {
 export const getUserProfile = () => {
   // 通過本地存儲 拿到用戶信息, 以便獲取 token數據
   // 將拿到的JSON格式字符串還原回 原來的數據對象
-  const user = JSON.parse(window.localStorage.getItem('user'))
-  console.log('本地存儲', user)
+  // const user = JSON.parse(window.localStorage.getItem('user'))
+  // console.log('本地存儲', user)
   return request({
     method: 'GET',
-    url: '/mp/v1_0/user/profile',
+    url: '/mp/v1_0/user/profile'
     // 後端要求 把需要授權的用戶身分 放到請求頭信息 中
     // axios 可以通過 headers 配置項 來設置請求頭信息
-    headers: {
-      // 屬性名和值都得看 後端接口的要求
-      // 1) 屬性名: Authorization, 後端接口 要求的
-      // 2) 屬性值: Bearar[空格][token數據]
-      // Bearar: 持票人
-      Authorization: `Bearer ${user.token}`
-    }
+    // headers: {
+    //   // 屬性名和值都得看 後端接口的要求
+    //   // 1) 屬性名: Authorization, 後端接口 要求的
+    //   // 2) 屬性值: Bearar[空格][token數據]
+    //   // Bearar: 持票人
+    //   // Authorization: `Bearer ${user.token}`
+    // }
   })
 }
--- src/utils/request.js
+++ src/utils/request.js
@@ -9,9 +9,33 @@ const request = axios.create({
   baseURL: 'http://api-toutiao-web.itheima.net' // 請求的基礎路徑
 })
 
-// 請求攔截器
+// axios 請求攔截器
+request.interceptors.request.use(
+  // 任何 所有的請求，都會經過這裡
+  // config 是當前請求 相關的配置信息對象
+  // config 是可以修改的
+  function (config) {
+    // 通過本地存儲 拿到用戶信息, 以便獲取 token數據
+    // 將拿到的JSON格式字符串還原回 原來的數據對象
+    const user = JSON.parse(window.localStorage.getItem('user'))
+    // console.log('本地存儲', user)
+    // 我們可以在允許請求 發送出去之前, 統一制訂 業務功能 處理需求
+    // 例如: 統一設置 token
+    // 如果有 登入用戶信息，則 統一設置 token
+    if (user) {
+      config.headers.Authorization = `Bearer ${user.token}`
+    }
+    console.log('配置信息', config)
+    // 當 return config 之後，請求 才會真正 發送出去
+    return config
+  },
+  // 請求失敗，會經過這裡
+  function (error) {
+    return Promise.reject(error)
+  }
+)
 
-// 響應攔截器
+// axios 響應攔截器
 
 // 導出 request 模塊
 export default request
