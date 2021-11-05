9496d3d07a8b39f0ffc18e92dddf43a9deb01c52
vihvch <vihvch@gmail.com>
2021-11-06 00:11:02 +0800

    在axios響應攔截器中，處理公共的錯誤狀態碼

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,6 +2,10 @@
  * 基於axios封裝的請求模塊
  */
 import axios from 'axios'
+import router from '@/router'
+
+// 非組件模塊可以 單獨引入使用 element-ui的Message提示組件
+import { Message } from 'element-ui'
 
 // 創建一個axios實例，說白了就是複製了一個axios
 // 我們通過這個實例去發請求，把需要的配置配置給這個實例來處理
@@ -36,6 +40,46 @@ request.interceptors.request.use(
 )
 
 // axios 響應攔截器
+// Add a response interceptor
+request.interceptors.response.use(
+  // Any status code that lie within the range of 2xx cause this function to trigger
+  // 任何 響應狀態碼 為2xx的響應，都會經過這裡 (表示 響應成功)
+  function (response) {
+    // Do something with response data
+    // response 是響應數據
+    return response
+  },
+  // Any status codes that falls outside the range of 2xx cause this function to trigger
+  // 任何 響應狀態碼 超過2xx的響應，都會經過這裡 (表示 響應失敗)
+  function (error) {
+    // Do something with response error
+    // console.log('響應狀態碼異常')
+    // console.dir(error)
+    // 處理公共的 錯誤狀態碼
+    const { status } = error.response
+    if (error.response) {
+      if (status === 401) {
+        // 登入狀態無效
+        // 清除本地存儲中的用戶登入狀態
+        window.localStorage.removeItem('user')
+        // 跳轉到 登入頁面
+        router.push('/login')
+        Message.warning('登入狀態無效, 請重新登入')
+      } else if (status === 403) {
+        // token 未攜帶 或已過期
+        // 客戶端 沒有訪問/操作權限
+        Message.warning('沒有操作權限')
+      } else if (status === 400) {
+        // 客戶端  請求參數錯誤
+        Message.error('參數異常, 請檢查請求參數')
+      } else if (status >= 500) {
+        // 服務端 內部錯誤/數據庫異常
+        Message.error('服務端內部異常, 請稍後重試')
+      }
+    }
+    return Promise.reject(error)
+  }
+)
 
 // 導出 request 模塊
 export default request
