bfbc77ebcc4bc3ccf11fd16295e5fc89b3befaed
vihvch <vihvch@gmail.com>
2021-12-04 10:53:48 +0800

    獲取文章列表

--- src/api/article.js
+++ src/api/article.js
@@ -14,3 +14,57 @@ export const getArticles = params => {
     params: params
   })
 }
+
+// 獲取文章頻道
+export const getArticleChannels = () => {
+  return request({
+    method: 'GET',
+    url: '/mp/v1_0/channels'
+  })
+}
+
+// 刪除文章
+export const deleteArticle = articleId => {
+  return request({
+    method: 'DELETE',
+    url: `/mp/v1_0/articles/${articleId}`
+  })
+}
+
+// 新建文章
+export const postArticle = (data, draft = false) => {
+  return request({
+    method: 'POST',
+    url: '/mp/v1_0/articles',
+    params: {
+      draft // 是否存為草稿 (true 為草稿)
+    },
+    data: data
+  })
+}
+
+// 編輯文章
+export const updateArticle = (articleId, data, draft = false) => {
+  return request({
+    method: 'PUT',
+    url: `/mp/v1_0/articles/${articleId}`,
+    params: {
+      draft // 是否存為草稿 (true 為草稿)
+    },
+    data: data
+  })
+}
+
+// 修改文章評論狀態
+export const updateCommentStatus = (articleId, allowComment) => {
+  return request({
+    method: 'PUT',
+    url: '/mp/v1_0/comments/status',
+    params: {
+      article_id: articleId
+    },
+    data: {
+      allow_Comment: allowComment
+    }
+  })
+}
