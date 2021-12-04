07c78a7e673f79dc81d29d5d8f6202b49933696e
vihvch <vihvch@gmail.com>
2021-12-04 11:01:29 +0800

    封裝echarts

---
+++ src/components/MyEcharts.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="my-echarts-container">
+    <!-- 為 ECharts 準備一個具備大小（寬高）的 DOM -->
+    <div ref="main" style="width: 600px; height: 400px;"></div>
+  </div>
+</template>
+
+<script>
+// import { nanoid } from 'nanoid'
+
+export default {
+  name: 'MyEcharts',
+  components: {},
+  props: {},
+  data () {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  created () {},
+  mounted () {},
+  methods: {}
+}
+</script>
+
+<style scoped lang="less"></style>
